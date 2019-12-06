b9f3d075c106fa19aaa1dcad253f3876054a3ab3
Alpha Phonecian <alpha.phonecian@gmail.com>
2019-12-06 11:26:06 +1100

    RPS Menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+
+use std::io;
+use std::io::{Write};
+
+
 fn main() {
-    println!("Hello, world!");
+    
+  print_banner();
+
+  print!(">> ");
+  io::stdout().flush();
+
+
+  read_line();
+
+}
+
+fn print_banner() {
+   println!("===============================");
+   println!("Rock Paper Scissors!");
+   println!("===============================");
+   println!("1) Rock");
+   println!("2) Paper");
+   println!("3) Scissors");
+}
+
+
+
+fn read_line() -> String {
+    let mut read = String::new();
+    io::stdin().read_line(&mut read).expect("Unable to read line.");
+    read.pop(); //remove the \n
+    read
 }
