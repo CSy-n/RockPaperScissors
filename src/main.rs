639211852e4f0ab1eac0b79845ceb7afaa0fe460
Henda <alpha.phonecian@gmail.com>
2019-12-09 22:39:52 +1100

    Progression #C

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,51 @@
 use std::io::{self, Write};
 use RockPaperScissors::util;
 
+
+
+
+/**
+  
+  Rock Paper Scissors:
+  :::::::::::::::::::::
+
+
+  A Rock Paper Scissors game, rules:
+   Rock beats Scissors
+   Paper beats Rock
+   Scissors beat Paper
+
+  
+  <|RPS Menu|>
+
+  ===============================
+  Rock Paper Scissors!
+  ===============================
+  1) Rock
+  2) Paper
+  3) Scissors
+  >>> <X>
+
+  |>RPS MENU<|
+
+  - Display Menu [RPS Menu]
+  - Take input from user
+  - Generate Random GameItem [Rock, Paper, Scissor]
+  - Compare result
+  - Display Results [Win, Loss]
+  - Return to Menu [RPS Menu]
+ 
+ */
+
+enum GameItem {
+  ROCK, PAPER, SCISSORS
+}
+
 fn main() {
     
   print_banner();
 
-  print!(">> ");
+  print!(">>> ");
   io::stdout().flush();
 
 
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,6 @@
 
 
+
 pub fn read_line() -> String {
     let mut read = String::new();
     std::io::stdin().read_line(&mut read).expect("Unable to read line.");
